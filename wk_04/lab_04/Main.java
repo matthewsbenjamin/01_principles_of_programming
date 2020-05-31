94a040eb33539d88d2c315916fb9209f56e17486
Ben Matthews <ben.matthews@seccl.tech>
2020-05-31 11:28:23 +0100

    Setup for lab 4

---
+++ wk_04/lab_04/Main.java
@@ -0,0 +1,16 @@
+
+/* 
+ * Lab Sheet 4
+ */
+
+/* Question 1
+ Make a BankAccount Class. This Class should provide a constructor, three mutators and three accessors of the type that a bank account might need. 
+ 
+ In the Main class (i.e. this class) create two different BankAccount instances from its main method (i.e. two BankAccount objects). Demonstrate the use of your mutators and accessors within that same main method.
+*/
+
+class Main {
+    public static void main(String[] args) {
+      System.out.println("Hello world!");
+    }
+  }
