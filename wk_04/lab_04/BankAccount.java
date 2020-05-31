7b8df0bad7ff9697f7345052120a65962060073d
Ben Matthews <ben.matthews@seccl.tech>
2020-05-31 21:25:47 +0100

    Completing lab 4

--- wk_04/lab_04/BankAccount.java
+++ wk_04/lab_04/BankAccount.java
@@ -1,3 +1,96 @@
 public class BankAccount {
+
+    private double balance;
+    private double creditLimit;
+    private String accName;
+
+    public BankAccount(final double bal, final double cl, final String accName) {
+        this.balance = bal;
+        this.creditLimit = cl;
+        this.accName = accName;
+        return;
+    }
+
+    private boolean LimitDetect(final double amount) {
+
+        // check whether transaction will exceed min/max double values
+        // not certain this works because of loss of information in the division step
+        double balUpper = this.balance / Double.MAX_VALUE;
+        double balLower = this.balance / Double.MIN_VALUE;
+        double amt = amount / Double.MAX_VALUE;
+
+        final boolean overflow = (balUpper + amt) > 1.0;
+        final boolean underflow = (balLower + amt) < -1.0;
+        
+        if (overflow) {
+            System.out.println("\tError: Transaction will exceed upper balance limits");
+            return true;
+        }
+        
+        if (underflow) {
+            System.out.println("\tError: Transaction will exceed lower  balance limits");
+            return true;
+        }
+        
+        
+        return false;
+    }
     
+    private boolean CreditCheck(double amount) {
+        final boolean creditLimitReached = (this.balance - amount) < -this.creditLimit;
+        if (creditLimitReached) {
+            System.out.println("\tCredit limit would be exceeded with this transaction.\n\tPlease add more funds to continue");
+            return true;
+        }
+        return false;
+    }
+
+    public double Balance() {
+        return this.balance;
+    }
+
+    public void Balance(int amt) {
+        this.balance = amt;
+    }
+
+    public String AccName() {
+        return this.accName;
+    }
+
+    private void TransactionHeader() {
+        System.out.printf("\n###################\nTransaction for account: %s\n", this.accName);
+    }
+
+    private void Credit(final double transaction) {
+        this.balance -= transaction;
+        return;
+    }
+
+    private void Debit(final double transaction) {
+        this.balance += transaction;
+        return;
+    }
+
+    public void TransferTo(BankAccount otherSide, double amount) {
+        this.TransactionHeader();
+        System.out.printf("\tTransaction to: %s\n", otherSide.accName);
+
+        if (LimitDetect(amount) || otherSide.LimitDetect(amount)) {
+            System.out.println("\tLimit detection failed");
+            return;
+        }
+
+        if (this.CreditCheck(amount)) {
+            return;
+        }
+
+        if (amount > 0) {
+            this.Credit(amount);
+            otherSide.Debit(amount);
+        } else {
+            System.out.println("\tAmount transferred cannot be less than zero");
+        }
+
+        System.out.printf("\tTransaction for £%f accepted\nYour balance is £%f\n", amount, this.balance);
+    }
 }
--- wk_04/lab_04/Main.java
+++ wk_04/lab_04/Main.java
@@ -1,4 +1,3 @@
-
 /* 
  * Lab Sheet 4
  */
@@ -9,8 +8,26 @@
  In the Main class (i.e. this class) create two different BankAccount instances from its main method (i.e. two BankAccount objects). Demonstrate the use of your mutators and accessors within that same main method.
 */
 
-class Main {
-    public static void main(String[] args) {
-      System.out.println("Hello world!");
+public class Main {
+    public static void main(final String[] args) {
+        BankAccount treasury = new BankAccount(0, 100000, "~ Treasury ~");
+        BankAccount a = new BankAccount(0, 1000, "Alex' debit account");
+        BankAccount b = new BankAccount(0, 500, "Boris' debit account");
+
+        // Money has to come from somewhere
+        // setting treasury w. loads of cash
+        treasury.Balance(1000);
+
+        treasury.TransferTo(a, 50);
+        treasury.TransferTo(b, 50);
+
+        a.TransferTo(b, 10);
+        b.TransferTo(treasury, 10);
+        b.TransferTo(b, 10);
+
+        System.out.println("Final Balances:");
+        System.out.printf("\t%s: £%f\n", treasury.AccName(), treasury.Balance());
+        System.out.printf("\t%s: £%f\n", a.AccName(), a.Balance());
+        System.out.printf("\t%s: £%f\n", b.AccName(), b.Balance());
     }
-  }
+}
