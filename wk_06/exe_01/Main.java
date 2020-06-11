e035108acde6591a809a311c112e457675b9785e
Ben Matthews <ben.matthews@seccl.tech>
2020-06-11 21:44:33 +0100

    Adds week six x01

---
+++ wk_06/exe_01/Main.java
@@ -0,0 +1,18 @@
+
+/* 
+ * Implement the class methods in MyMath class. 
+ *
+ * Test your implementations using the main method below
+ */ 
+
+class Main {
+    public static void main(String[] args) {
+      System.out.println(MyMath.add(3,2));
+      System.out.println(MyMath.sub(3,2));
+      System.out.println(MyMath.mult(3,2));
+      System.out.println(MyMath.div(3,2));
+      System.out.println(MyMath.pow(3,2));
+      System.out.println(MyMath.sqrt(3));
+      System.out.println(MyMath.PI);
+    }
+  }
---
+++ wk_06/exe_01/MyMath.java
@@ -0,0 +1,36 @@
+
+public class MyMath{
+
+    public static final double PI = 3.14;
+  
+    // addition 
+    public static int add(int x, int y){
+      return x + y;
+    }
+  
+    // subtraction 
+    public static int sub(int x, int y){
+      return x - y;
+    }
+  
+    // multiplication
+    public static int mult(int x, int y){
+      return x * y;
+    }
+  
+    // division 
+    public static int div(int x, int y){
+      return x / y;
+    }
+  
+    // raise x to the power y - x^y
+    public static int pow(int x, int y){
+      return x ^ y;
+    }
+  
+    // square root
+    public static int sqrt(int x){
+      return x * x;
+    }
+  
+  }
